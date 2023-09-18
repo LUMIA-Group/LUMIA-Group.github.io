7e35ba86f578912205d1cf15c68eaceafa64e6ff
guojia <505610340@qq.com>
2023-09-18 16:00:48 +0800

    build test

--- src/data/home.js
+++ src/data/home.js
@@ -2,7 +2,7 @@
 // 按规则修改和添加内容即可，请不要更改数据结构以及变量命名
 export const homeData = {
     // 实验室网站名称
-    name: 'Name',
+    name: 'New Name',
     // 实验室介绍
     desc: 'Our latest web design tips, tricks, insights, and resources, hot offthe presses.',
     // 相关介绍的图片及文字
