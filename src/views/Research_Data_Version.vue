41cf4b1ba69b564f3fe3dc1a9a7293df4f0ccb29
LinChen——Macbook Air <lchen>
2026-03-02 18:55:04 +0800

    chore(research): 规范 Research_Data_Version.vue 结构格式
    - 重排论文列表模板嵌套结构与链接渲染代码\n- 统一脚本区 import/data 代码风格\n- 修正样式区缩进与分号，提升整体一致性

--- src/views/Research_Data_Version.vue
+++ src/views/Research_Data_Version.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="research-container">
     <div class="research-content">
-      <el-row v-for="(item, index) in researchData.researchList" :key="index" :gutter="36">
+      <el-row
+        v-for="(item, index) in researchData.researchList"
+        :key="index"
+        :gutter="36"
+      >
         <el-col :span="6">
           <img :src="item.img" width="100%" alt="" srcset="" />
         </el-col>
@@ -10,11 +14,13 @@
             <h3 class="name">{{ item.name }}</h3>
             <p class="author">{{ item.author }}</p>
             <div class="list">
-                <span v-for="(l, lindex) in item.list" :key="lindex">
-                    <span v-if="!l.link">{{ l.name }}</span>
-                    <a v-else :href="l.link">{{l.name}}</a>
-                    <span v-if="lindex !== item.list.length - 1" class="list-gap"> | </span>
+              <span v-for="(l, lindex) in item.list" :key="lindex">
+                <span v-if="!l.link">{{ l.name }}</span>
+                <a v-else :href="l.link">{{ l.name }}</a>
+                <span v-if="lindex !== item.list.length - 1" class="list-gap">
+                  |
                 </span>
+              </span>
             </div>
             <article class="abstract">
               {{ item.abstract }}
@@ -26,11 +32,11 @@
   </div>
 </template>
 <script>
-import { researchData } from '@/data/research'
+import { researchData } from "@/data/research";
 export default {
   data() {
     return {
-        researchData: researchData
+      researchData: researchData,
     };
   },
 };
@@ -45,7 +51,7 @@ export default {
     padding: 48px 0;
     border-bottom: 1px solid #004d4c;
     &:last-child {
-        border-bottom: none
+      border-bottom: none;
     }
   }
   .name {
@@ -64,13 +70,13 @@ export default {
     text-align: justify;
   }
   .list {
-      margin-bottom: 16px;
-      a {
-          color: #004d4c;;
-      }
-      .list-gap {
-          padding: 0 6px;
-      }
+    margin-bottom: 16px;
+    a {
+      color: #004d4c;
+    }
+    .list-gap {
+      padding: 0 6px;
+    }
   }
 }
 </style>
